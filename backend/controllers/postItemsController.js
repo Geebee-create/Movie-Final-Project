46fc3991f4b62853807210024781cf5e41ee09cf
Geebee-create <156425632+Geebee-create@users.noreply.github.com>
2024-03-21 10:48:52 +0000

    Current changes to backend but still working on this.
    added some new files.

--- backend/controllers/postItemsController.js
+++ backend/controllers/postItemsController.js
@@ -2,8 +2,7 @@
 // THE VIDS FROM WHEN WE BUILT THIS FILE are 'Fullstack exercise- 20240308_140119' from 03-08,
 // and then 'Full Stack Cont-20240311_100628' from 03-11.
 
-const Post = require ("../models/post")
-// the models file still needs to be completed as of 20/03/2024. Does this need to be /post or something else?- Gina
+const Post = require ("../models/postSchema.js")
 
 // createPost can also be seen in itemsRoute.js
 const createPost = async (req, res) =>  {
@@ -20,13 +19,8 @@ const createPost = async (req, res) =>  {
     res.status(200).json(newPost)
 }
 
-// createComment can also be seen in itemsRoute.js
-const createComment = async (req, res) =>  {
-    const { text } = req.body
-    console.log(text)
-    const commentObject = new Comment({
-        text
-    })
-    const newComment = await commentObject.save()
-    res.status(200).json(newComment)
+
+
+module.exports = {
+    createPost
 }
---
+++ backend/models/postSchema.js
@@ -0,0 +1,19 @@
+// this schema will be to send posts/reviews to the Posts collection in the database.
+
+// this file defines what the data looks like 
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const postSchema = new Schema({
+    text: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Posts', postSchema)
+
+// example of how module.exports line looked for the fullstack todo app:
+// module.exports = mongoose.model('Todo', todoSchema)
+
