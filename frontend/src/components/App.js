04e2e27e6479702160f909ecc4a60727cb888656
Geebee-create <156425632+Geebee-create@users.noreply.github.com>
2024-03-27 14:26:33 +0000

    Nav bar sorted
    Nav bar now works

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,26 +1,25 @@
-import React, { useState, useEffect } from "react";
-// import Navbar from "./Navbar";
-import Home from "../components/homepage";
-
-
-import Log from "../login/Sign";
-
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./homepage";
+import Log from "../login/Sign";
+import BattlePage from "../components/BattlePage"; 
+import Profile from "../components/profile"; 
+import About from "../components/about"; 
 
 const App = () => {
-
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/log" element={<Log />}></Route>
-
-          <Route path="/" exact element={<Home />}></Route>
+          <Route path="/log" element={<Log />} />
+          <Route path="/" exact element={<Home />} />
+          <Route path="/Battle-Page" element={<BattlePage />} /> 
+          <Route path="/profile" element={<Profile />} /> 
+          <Route path="/about" element={<About />} /> 
         </Routes>
       </BrowserRouter>
     </div>
   );
 };
-
 
 export default App;
